3e6cc26f3374f98a1c1ed44ad65ff7e9321e0473
Madela06 <Brianmadela2@gmail.com>
2025-07-21 16:55:40 +0200

    updated homrpage

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -5,7 +5,10 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class HomePage:
 
-    lbl_welcome_xpath="//*[contains(text(), 'SPORT')]"
+    lbl_SPORT_xpath="//*[contains(text(), 'SPORT')]"
+    tab_Formula1_xpath="//span[text()='Formula 1']"
+    tab_Results_xpath="//span[text()='Results']"
+    tab_2023_xpath="//span[text()='2023']"
 
     def __init__(self, driver):
         self.driver = driver
@@ -14,3 +17,18 @@ class HomePage:
         wait=WebDriverWait(self.driver, 10)
         element = wait.until(EC.visibility_of_element_located((By.XPATH, self.lbl_welcome_xpath)))
         element.is_displayed()
+
+    def clickFormula1(self):
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.visibility_of_element_located((By.XPATH, self.tab_Formula1_xpath)))
+        element.click()
+
+    def clickResults(self):
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.visibility_of_element_located((By.XPATH, self.tab_Results_xpath)))
+        element.click()
+
+    def click2023(self):
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.visibility_of_element_located((By.XPATH, self.tab_2023_xpath)))
+        element.click()
--- Test/Test_Formula1Results.py
+++ Test/Test_Formula1Results.py
@@ -10,6 +10,8 @@ class Test_Formula1Results:
         self.driver = setup
         self.driver.get(self.url)
 
+
+
         # Assuming there's a method to verify results on the page
         # This is a placeholder for actual verification logic
         #assert "Results" in self.driver.title, "Formula 1 Results page did not load correctly"
