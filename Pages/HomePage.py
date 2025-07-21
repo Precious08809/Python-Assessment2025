7deb2e69419865b7d620ca20ed7e99d841acc5e6
Madela06 <Brianmadela2@gmail.com>
2025-07-21 19:57:45 +0200

    added method verify results

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -4,17 +4,17 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class HomePage:
-
-    lbl_SPORT_xpath="//*[contains(text(), 'SPORT')]"
-    tab_Formula1_xpath="//span[text()='Formula 1']"
-    tab_Results_xpath="//span[text()='Results']"
-    tab_2023_xpath="//span[text()='2023']"
+    lbl_SPORT_xpath = "//*[contains(text(), 'SPORT')]"
+    tab_Formula1_xpath = "//span[text()='Formula 1']"
+    tab_Results_xpath = "//span[text()='Results']"
+    tab_2023_xpath = "//span[text()='2023']"
+    tab_lasvegas_xpath = "//span[text()='Las Vegas Grand Prix, Las Vegas Street Circuit']"
 
     def __init__(self, driver):
         self.driver = driver
 
     def verifySportIsDisplayed(self):
-        wait=WebDriverWait(self.driver, 10)
+        wait = WebDriverWait(self.driver, 10)
         element = wait.until(EC.visibility_of_element_located((By.XPATH, self.lbl_welcome_xpath)))
         element.is_displayed()
 
@@ -32,3 +32,12 @@ class HomePage:
         wait = WebDriverWait(self.driver, 10)
         element = wait.until(EC.visibility_of_element_located((By.XPATH, self.tab_2023_xpath)))
         element.click()
+
+    def verifyFormula1Text(self):
+        pass
+
+
+def clickLasVegas(self):
+    wait = WebDriverWait(self.driver, 10)
+    element = wait.until(EC.visibility_of_element_located((By.XPATH, self.tab_lasvegas_xpath)))
+    element.click()
