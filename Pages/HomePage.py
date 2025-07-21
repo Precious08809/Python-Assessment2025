0a9fa01a4eb8e3cd549c201459a0fb4501fb61a0
Nkosi <njceles@gmail.com>
2025-07-21 20:25:16 +0200

    clicked formular 1

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -9,6 +9,10 @@ class HomePage:
     tab_Results_xpath = "//span[text()='Results']"
     tab_2023_xpath = "//span[text()='2023']"
     tab_lasvegas_xpath = "//span[text()='Las Vegas Grand Prix, Las Vegas Street Circuit']"
+    RESULTS_TEXT_XPATH = "//span[@class='ssrcss-1mstwv3-LinkTextContainer eis6szr1' and text()='Results']"
+    YEAR_2023_XPATH = "//div[@data-content='2023' and @type='year' and text()='2023']"
+    ABU_DHABI_GP_XPATH = "//span[@class='ssrcss-6v626p-DrawerHandleTitle elwr9uk5' and text()='Abu Dhabi Grand Prix, Yas Marina']"
+    LAS_VEGAS_GP_XPATH = "//span[@class='ssrcss-6v626p-DrawerHandleTitle elwr9uk5' and text()='Las Vegas Grand Prix, Las Vegas Street Circuit']"
 
     def __init__(self, driver):
         self.driver = driver
@@ -36,8 +40,23 @@ class HomePage:
     def verifyFormula1Text(self):
         pass
 
+    def clickLasVegas(self):
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.visibility_of_element_located((By.XPATH, self.tab_lasvegas_xpath)))
+        element.click()
 
-def clickLasVegas(self):
-    wait = WebDriverWait(self.driver, 10)
-    element = wait.until(EC.visibility_of_element_located((By.XPATH, self.tab_lasvegas_xpath)))
-    element.click()
+    def clickResults(self):
+        results_element = self.driver.find_element(By.XPATH, self.RESULTS_TEXT_XPATH)
+        results_element.click()
+
+    def clickYear2023(self):
+        year_2023_element = self.driver.find_element(By.XPATH, self.YEAR_2023_XPATH)
+        year_2023_element.click()
+
+    def clickAbuDhabiGrandPrix(self):
+        abu_dhabi_gp_element = self.driver.find_element(By.XPATH, self.ABU_DHABI_GP_XPATH)
+        abu_dhabi_gp_element.click()
+
+    def clickLasVegasGrandPrix(self):
+        las_vegas_gp_element = self.driver.find_element(By.XPATH, self.LAS_VEGAS_GP_XPATH)
+        las_vegas_gp_element.click()
